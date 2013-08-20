b5676224b2426136da8968b49acbc871c2b6eea1
braungoodson <braungoodson@gmail.com>
2013-08-19 20:29:03 -0400

    formatting

--- httpServer/restApi/microMrpRestApi.js
+++ httpServer/restApi/microMrpRestApi.js
@@ -8,10 +8,30 @@ module.exports = {
 		this.router = router;
 		this.fileSystem = require('fs');
 		this.requestHandlers = require('./requestHandlers/microMrpRestApiRequestHandlers');
-		this.router.get('/',this.requestHandlers.rootRequestHandler(this.fileSystem,this.requestHandlers.rootRequestErrorHandler));
-		this.router.get('/api/materials',this.requestHandlers.materialsRequestHandler(this.dbConnector.schemaModels.models.material,this.requestHandlers.materialsRequestErrorHandler));
-		this.router.post('/api/cr/material',this.requestHandlers.createMaterialRequestHandler(this.dbConnector.schemaModels.models.material,this.requestHandlers.basicErrorHandler));
-		this.router.put('/api/u/material',this.requestHandlers.updateMaterialByIdRequestHandler(this.dbConnector.schemaModels.models.material,this.requestHandlers.basicErrorHandler));
-		this.router.delete('/api/d/material',this.requestHandlers.deleteMaterialById(this.dbConnector.schemaModels.models.material,this.requestHandlers.basicErrorHandler));
+		this.router.get('/',
+			this.requestHandlers.rootRequestHandler(
+				this.fileSystem,
+				this.requestHandlers.rootRequestErrorHandler
+		));
+		this.router.get('/api/materials',
+			this.requestHandlers.materialsRequestHandler(
+				this.dbConnector.schemaModels.models.material,
+				this.requestHandlers.materialsRequestErrorHandler
+		));
+		this.router.post('/api/cr/material',
+			this.requestHandlers.createMaterialRequestHandler(
+				this.dbConnector.schemaModels.models.material,
+				this.requestHandlers.basicErrorHandler
+		));
+		this.router.put('/api/u/material',
+			this.requestHandlers.updateMaterialByIdRequestHandler(
+				this.dbConnector.schemaModels.models.material,
+				this.requestHandlers.basicErrorHandler
+		));
+		this.router.delete('/api/d/material',
+			this.requestHandlers.deleteMaterialById(
+				this.dbConnector.schemaModels.models.material,
+				this.requestHandlers.basicErrorHandler
+		));
 	}
 }
